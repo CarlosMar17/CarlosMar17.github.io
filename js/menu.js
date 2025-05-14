a1d8cfda922bb1c32fc4eb8660b90e83e4fab28f
Carlos Mar <eduardo.marrdrgz@uanl.edu.mx>
2025-05-14 08:49:18 -0600

    Actualizacion menu

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,18 @@ const menu = document.querySelector("#menu");
 btnMenu.addEventListener("click", function() {
           menu.classList.toggle("mostrar");
 });
+
+$(document).ready(function() {
+    // Obtén la ruta actual, por ejemplo: /pages/Solar.html
+    const currentPath = window.location.pathname.split("/").pop();
+  
+    // Recorre todos los enlaces del menú
+    $(".menu a").each(function() {
+      const linkPath = $(this).attr("href");
+  
+      // Si el enlace coincide con la ruta actual, añade la clase 'active'
+      if (linkPath === currentPath) {
+        $(this).addClass("active");
+      }
+    });
+  });
